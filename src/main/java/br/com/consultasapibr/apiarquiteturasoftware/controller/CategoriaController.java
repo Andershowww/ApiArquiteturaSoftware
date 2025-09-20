5db32aa7c00a01c7d748a77995a72a46f527d188
devsantos16 <devsantos1999@gmail.com>
2025-09-20 17:42:18 -0300

    adicionando categoria pra consumo da api

---
+++ src/main/java/br/com/consultasapibr/apiarquiteturasoftware/controller/CategoriaController.java
@@ -0,0 +1,27 @@
+package br.com.consultasapibr.apiarquiteturasoftware.controller;
+
+import java.util.List;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import br.com.consultasapibr.apiarquiteturasoftware.dto.CategoriaDTO;
+import br.com.consultasapibr.apiarquiteturasoftware.service.CategoriaService;
+
+@RestController
+@RequestMapping("/categorias")
+public class CategoriaController {
+    private final CategoriaService service;
+
+    public CategoriaController(CategoriaService service) {
+        this.service = service;
+    }
+
+    @GetMapping
+    public ResponseEntity<List<CategoriaDTO>> listarCategorias() {
+        List<CategoriaDTO> categoria = service.listarCategorias();
+        return ResponseEntity.ok(categoria);
+    }
+}
