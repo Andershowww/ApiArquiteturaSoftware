a238787539fb93f526df8860dc639b9271f3c5f8
LUIS RICARDO <ricardu@rede.ulbra.br>
2025-09-23 13:14:43 -0300

    Implementação de autenticação OAuth2

---
+++ src/main/java/br/com/consultasapibr/apiarquiteturasoftware/config/SecurityConfig.java
@@ -0,0 +1,28 @@
+package br.com.consultasapibr.apiarquiteturasoftware.config;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.web.SecurityFilterChain;
+
+@Configuration
+public class SecurityConfig {
+
+    @Bean
+    public SecurityFilterChain securityFilterChain(HttpSecurity http) throws Exception {
+        http
+            .authorizeHttpRequests(auth -> auth
+                .requestMatchers("/public/**", "/swagger-ui/**", "/v3/api-docs/**").permitAll()
+                .anyRequest().authenticated()
+            )
+            .oauth2Login(oauth2 -> oauth2
+                .loginPage("/oauth2/authorization/google") // ou github
+                .defaultSuccessUrl("http://localhost:3000", true)
+            )
+            .logout(logout -> logout
+                .logoutSuccessUrl("/public/logout-success").permitAll()
+            );
+
+        return http.build();
+    }
+}
