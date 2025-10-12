ec8657b15918e958fe52794bc0b6eda31194b1b7
anderson <anderson@petshopcontrol.com.br>
2025-10-12 13:45:39 -0300

    arumando protecao das rotas da api

--- src/test/java/br/com/gestaoproducao/FornecedorApiTest.java
+++ src/test/java/br/com/gestaoproducao/FornecedorApiTest.java
@@ -11,7 +11,7 @@ import static org.springframework.test.web.servlet.request.MockMvcRequestBuilder
 import static org.springframework.test.web.servlet.request.MockMvcRequestBuilders.post;
 import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.*;
 
-@SpringBootTest(classes = gestaoproducao.class)
+@SpringBootTest(classes = GestaoProducao.class)
 @AutoConfigureMockMvc
 class FornecedorApiTest {
 
