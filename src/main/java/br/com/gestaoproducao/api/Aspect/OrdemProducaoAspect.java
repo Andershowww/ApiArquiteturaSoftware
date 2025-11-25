fa7a1c95963c804e93e6d4aa902261577028fee4
devsantos16 <devsantos1999@gmail.com>
2025-11-25 20:39:50 -0300

    feat: adicionando um exemplo de aspect

---
+++ src/main/java/br/com/gestaoproducao/api/Aspect/OrdemProducaoAspect.java
@@ -0,0 +1,17 @@
+package br.com.gestaoproducao.api.Aspect;
+
+
+import org.aspectj.lang.JoinPoint;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Before;
+import org.springframework.stereotype.Component;
+
+@Aspect
+@Component
+public class OrdemProducaoAspect {
+    @Before("execution(* br.com.gestaoproducao.api.service.*.gerarOrdemProducao(..))")
+    public void logAntes(JoinPoint joinPoint) {
+        Object[] args = joinPoint.getArgs();
+        System.out.println("Gerando ordem de produção com DTO: " + args[0]);
+    }
+}
